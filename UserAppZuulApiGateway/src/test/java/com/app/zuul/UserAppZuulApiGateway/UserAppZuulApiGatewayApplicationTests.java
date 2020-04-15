bdda1ef0eb8d5d1e19cf99cb2732dc14577a38de
Akansh <akansh_rai@epam.com>
2020-04-15 09:49:12 +0530

    Zuul Api gateway for application

---
+++ UserAppZuulApiGateway/src/test/java/com/app/zuul/UserAppZuulApiGateway/UserAppZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.zuul.UserAppZuulApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
