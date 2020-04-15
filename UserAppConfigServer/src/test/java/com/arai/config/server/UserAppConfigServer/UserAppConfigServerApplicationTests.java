4b70077b36c18bec75309a691df6d97107043ec0
Akansh <akansh_rai@epam.com>
2020-04-15 09:47:35 +0530

    Central config server for UserSearchMicroServiceProject

---
+++ UserAppConfigServer/src/test/java/com/arai/config/server/UserAppConfigServer/UserAppConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arai.config.server.UserAppConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAppConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
